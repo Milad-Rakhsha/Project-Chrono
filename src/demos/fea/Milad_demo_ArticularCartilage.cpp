7ff6a9ffe8e3339edaa22f9abf1d68c42f0d0873
Milad-Rakhsha <rakhsha@wisc.edu>
2016-03-01 14:53:22 -0600

    Stable simulation with : 1- 1 MPa pressure on Tibia cartilage 2- E=15 MPa 3- stiffness and Damping factors corresponde to what was reported in the literature.

--- src/demos/fea/Milad_demo_ArticularCartilage.cpp
+++ src/demos/fea/Milad_demo_ArticularCartilage.cpp
@@ -57,15 +57,122 @@ using namespace std;
 // bool addConstrain = true;
 // bool addForce = true;
 bool addGravity = false;
-bool addPressure = false;
+bool addPressure = true;
 bool addForce = true;
 bool showTibia = false;
-bool showFemur = true;
+bool showFemur = false;
 // bool addFixed = false;
 double time_step = 0.0001;
 int scaleFactor = 1;
-double dz = 0.0005;
+double dz = 0.001;
 double MeterToInch = 0.02539998628;
+double L0 = 0.1;  // Initial length
+double L0_t = 0.1;
+class MyLoadSpringDamper : public ChLoadCustomMultiple {
+  public:
+    MyLoadSpringDamper(std::vector<std::shared_ptr<ChLoadable>>& mloadables, std::shared_ptr<ChBody> AttachBodyInput)
+        : ChLoadCustomMultiple(mloadables),
+          AttachBody(AttachBodyInput),
+          K_sp(mloadables.size(), 1e4),
+          C_dp(mloadables.size(), 1e2),
+          LocalBodyAtt(mloadables.size(), AttachBody->GetPos()) {
+        assert(std::dynamic_pointer_cast<ChNodeFEAxyzD>(mloadables[0]));
+        loadables.push_back(AttachBody);
+        load_Q.Reset(this->LoadGet_ndof_w());
+        for (size_t i = 0; i < mloadables.size(); i++) {
+            auto Node = std::static_pointer_cast<ChNodeFEAxyzD>(mloadables[i]);
+            l0.push_back((Node->GetPos() - AttachBody->GetPos()).Length());
+        }
+    }
+
+    std::shared_ptr<ChBody> AttachBody;    // Body to attach springs and dampers
+    std::vector<double> K_sp;              // Stiffness of springs
+    std::vector<double> C_dp;              // Damping coefficient of dampers
+    std::vector<double> l0;                // Initial, undeformed spring-damper length
+    std::vector<ChVector<>> LocalBodyAtt;  // Local Body Attachment
+
+    virtual void ComputeQ(ChState* state_x,      ///< state position to evaluate Q
+                          ChStateDelta* state_w  ///< state speed to evaluate Q
+                          ) {
+        ChVector<> Node_Pos;
+        ChVector<> Node_Vel;
+        ChVector<> Node_Grad;
+
+        ChVector<> dij;   // Vector describing current spring-damper relative location
+        ChVector<> ddij;  // Vector describing current spring-damper relative velocity
+
+        ChVector<> NodeAttachment;
+        if (state_x && state_w) {
+            // GetLog() << " We are reaching this" << loadables.size();
+            for (int iii = 0; iii < loadables.size() - 1; iii++) {
+                Node_Pos = state_x->ClipVector(iii * 6, 0);
+                Node_Grad = state_x->ClipVector(iii * 6 + 3, 0);
+                Node_Vel = state_w->ClipVector(iii * 6, 0);
+
+                ChVector<> BodyAttachWorld = AttachBody->Point_Body2World(LocalBodyAtt[iii]);
+                dij = Node_Pos - BodyAttachWorld;  // Current relative vector between attachment points
+                double c_length = dij.Length();    // l
+                ddij = Node_Vel - AttachBody->PointSpeedLocalToParent(LocalBodyAtt[iii]);  // Time derivative of dij
+                double dc_length = 1 / c_length * (dij.x * ddij.x + dij.y * ddij.y + dij.z * ddij.z);  // ldot
+                double for_spdp =
+                    K_sp[iii] * (c_length - l0[iii]) + C_dp[iii] * dc_length;  // Absolute value of spring-damper force
+
+                // Apply generalized force to node
+                //    GetLog() << " Vertical component and absolute value of force: " << dij.GetNormalized().y << "  "
+                //    << for_spdp;
+                this->load_Q(iii * 6 + 0) = -for_spdp * dij.GetNormalized().x;
+                this->load_Q(iii * 6 + 1) = -for_spdp * dij.GetNormalized().y;
+                this->load_Q(iii * 6 + 2) = -for_spdp * dij.GetNormalized().z;
+
+                ChVectorDynamic<> Qi(6);  // Vector of generalized forces from spring and damper
+                ChVectorDynamic<> Fi(6);  // Vector of applied forces and torques (6 components)
+                double detJi = 0;         // Determinant of transformation (Not used)
+
+                Fi(0) = for_spdp * dij.GetNormalized().x;
+                Fi(1) = for_spdp * dij.GetNormalized().y;
+                Fi(2) = for_spdp * dij.GetNormalized().z;
+                Fi(3) = 0.0;
+                Fi(4) = 0.0;
+                Fi(5) = 0.0;
+
+                ChState stateBody_x(7, NULL);
+                ChStateDelta stateBody_w(6, NULL);
+
+                stateBody_x(0) = (*state_x)((loadables.size() - 1) * 6);
+                stateBody_x(1) = (*state_x)((loadables.size() - 1) * 6 + 1);
+                stateBody_x(2) = (*state_x)((loadables.size() - 1) * 6 + 2);
+                stateBody_x(3) = (*state_x)((loadables.size() - 1) * 6 + 3);
+                stateBody_x(4) = (*state_x)((loadables.size() - 1) * 6 + 4);
+                stateBody_x(5) = (*state_x)((loadables.size() - 1) * 6 + 5);
+                stateBody_x(6) = (*state_x)((loadables.size() - 1) * 6 + 6);
+
+                stateBody_w(0) = (*state_w)((loadables.size() - 1) * 6);
+                stateBody_w(1) = (*state_w)((loadables.size() - 1) * 6 + 1);
+                stateBody_w(2) = (*state_w)((loadables.size() - 1) * 6 + 2);
+                stateBody_w(3) = (*state_w)((loadables.size() - 1) * 6 + 3);
+                stateBody_w(4) = (*state_w)((loadables.size() - 1) * 6 + 4);
+                stateBody_w(5) = (*state_w)((loadables.size() - 1) * 6 + 5);
+
+                // Apply generalized force to rigid body (opposite sign)
+                AttachBody->ComputeNF(BodyAttachWorld.x, BodyAttachWorld.y, BodyAttachWorld.z, Qi, detJi, Fi,
+                                      &stateBody_x, &stateBody_w);
+
+                // Apply forces to body (If body fixed, we should set those to zero not Qi(coordinate))
+                this->load_Q((loadables.size() - 1) * 6) = 0.0;
+                this->load_Q((loadables.size() - 1) * 6 + 1) = 0.0;
+                this->load_Q((loadables.size() - 1) * 6 + 2) = 0.0;
+                this->load_Q((loadables.size() - 1) * 6 + 3) = 0.0;
+                this->load_Q((loadables.size() - 1) * 6 + 4) = 0.0;
+                this->load_Q((loadables.size() - 1) * 6 + 5) = 0.0;
+            }
+        } else {
+            // explicit integrators might call ComputeQ(0,0), null pointers mean
+            // that we assume current state, without passing state_x for efficiency
+        }
+    }
+    // Remember to set this as stiff, to enable the jacobians
+    virtual bool IsStiff() { return false; }
+};
 
 int main(int argc, char* argv[]) {
     ChSystemDEM my_system;
@@ -90,7 +197,7 @@ int main(int argc, char* argv[]) {
     double sphere_swept_thickness = dz * 0.01;
 
     double rho = 1000;  ///< material density
-    double E = 5e6;     ///< Young's modulus
+    double E = 15e6;    ///< Young's modulus
     double nu = 0.3;    ///< Poisson ratio
     // Create the surface material, containing information
     // about friction etc.
@@ -149,6 +256,7 @@ int main(int argc, char* argv[]) {
     GetLog() << "-----------------------------------------------------------\n\n";
 
     int TotalNumNodes, TotalNumElements, TottalNumBEdges;
+    std::vector<double> NODE_AVE_AREA;
     std::vector<int> BC_NODES;
     std::vector<int> BC_NODES1;
     std::vector<int> BC_NODES2;
@@ -164,40 +272,45 @@ int main(int argc, char* argv[]) {
 
     ChMatrix33<> rot_transform(1);
     // Import the Torus
-    try {
-        ChMeshFileLoader::ANCFShellFromGMFFile(my_mesh, GetChronoDataFile("fea/FemurFine.mesh").c_str(), material,
-                                               BC_NODES, Center_Femur, rot_transform, MeterToInch, false, false);
-    } catch (ChException myerr) {
-        GetLog() << myerr.what();
-        return 0;
-    }
+    //    try {
+    //        ChMeshFileLoader::ANCFShellFromGMFFile(my_mesh, GetChronoDataFile("fea/Femur.mesh").c_str(), material,
+    //                                               NODE_AVE_AREA, BC_NODES, Center_Femur, rot_transform, MeterToInch,
+    //                                               false,
+    //                                               false);
+    //    } catch (ChException myerr) {
+    //        GetLog() << myerr.what();
+    //        return 0;
+    //    }
+    //    //
+    //    if (true) {
+    //        for (int node = 0; node < BC_NODES.size(); node++) {
+    //            auto NodePosBone = std::make_shared<ChLinkPointFrame>();
+    //            auto NodeDirBone = std::make_shared<ChLinkDirFrame>();
+    //            auto ConstrainedNode = std::make_shared<ChNodeFEAxyzD>();
     //
-    if (true) {
-        for (int node = 0; node < BC_NODES.size(); node++) {
-            auto NodePosBone = std::make_shared<ChLinkPointFrame>();
-            auto NodeDirBone = std::make_shared<ChLinkDirFrame>();
-            auto ConstrainedNode = std::make_shared<ChNodeFEAxyzD>();
+    //            ConstrainedNode = std::dynamic_pointer_cast<ChNodeFEAxyzD>(my_mesh->GetNode(BC_NODES[node]));
+    //            NodePosBone->Initialize(ConstrainedNode, Femur);
+    //            my_system.Add(NodePosBone);
+    //
+    //            NodeDirBone->Initialize(ConstrainedNode, Femur);
+    //            NodeDirBone->SetDirectionInAbsoluteCoords(ConstrainedNode->D);
+    //            my_system.Add(NodeDirBone);
+    //        }
+    //    }
+    int Femur_mesh_Nnodes = my_mesh->GetNnodes();
+    //
+    //    //    ChMatrix33<> rot_transform_2(1);
+    //    //    rot_transform_2.SetElement(0, 0, 1);
+    //    //    rot_transform_2.SetElement(1, 1, -1);
+    //    //    rot_transform_2.SetElement(2, 2, -1);
+    //    //    Center = ChVector<>(0, -0.2, 0.4);
 
-            ConstrainedNode = std::dynamic_pointer_cast<ChNodeFEAxyzD>(my_mesh->GetNode(BC_NODES[node]));
-            NodePosBone->Initialize(ConstrainedNode, Femur);
-            my_system.Add(NodePosBone);
-
-            NodeDirBone->Initialize(ConstrainedNode, Femur);
-            NodeDirBone->SetDirectionInAbsoluteCoords(ConstrainedNode->D);
-            my_system.Add(NodeDirBone);
-        }
-    }
-
-    //    ChMatrix33<> rot_transform_2(1);
-    //    rot_transform_2.SetElement(0, 0, 1);
-    //    rot_transform_2.SetElement(1, 1, -1);
-    //    rot_transform_2.SetElement(2, 2, -1);
-    //    Center = ChVector<>(0, -0.2, 0.4);
     ChVector<> Center(0, 0.0, 0);
     // Import the mesh
     try {
         ChMeshFileLoader::ANCFShellFromGMFFile(my_mesh, GetChronoDataFile("fea/Tibia-1.mesh").c_str(), material,
-                                               BC_NODES1, Center, rot_transform, MeterToInch, false, false);
+                                               NODE_AVE_AREA, BC_NODES1, Center, rot_transform, MeterToInch, false,
+                                               false);
     } catch (ChException myerr) {
         GetLog() << myerr.what();
         return 0;
@@ -211,7 +324,8 @@ int main(int argc, char* argv[]) {
     // Import the mesh
     try {
         ChMeshFileLoader::ANCFShellFromGMFFile(my_mesh, GetChronoDataFile("fea/Tibia-2.mesh").c_str(), material,
-                                               BC_NODES2, Center, rot_transform, MeterToInch, false, false);
+                                               NODE_AVE_AREA, BC_NODES2, Center, rot_transform, MeterToInch, false,
+                                               false);
     } catch (ChException myerr) {
         GetLog() << myerr.what();
         return 0;
@@ -222,6 +336,43 @@ int main(int argc, char* argv[]) {
         FixedNode->SetFixed(true);
     }
 
+    /// TEST TO INCLUDE SPRING-DAMPER GENERALIZED FORCES
+    auto mloadcontainerGround = std::make_shared<ChLoadContainer>();
+    double Tottal_stiff = 0;
+    double Tottal_damp = 0;
+
+    if (true) {
+        // Select on which nodes we are going to apply a load
+        std::vector<std::shared_ptr<ChLoadable>> NodeList;
+        for (int iNode = 0; iNode < my_mesh->GetNnodes(); iNode++) {
+            auto NodeLoad = std::dynamic_pointer_cast<ChNodeFEAxyzD>(my_mesh->GetNode(iNode));
+            NodeList.push_back(NodeLoad);
+        }
+        auto OneLoadSpringDamper = std::make_shared<MyLoadSpringDamper>(NodeList, Tibia);
+        for (int iNode = Femur_mesh_Nnodes; iNode < my_mesh->GetNnodes(); iNode++) {
+            auto Node = std::dynamic_pointer_cast<ChNodeFEAxyzD>(my_mesh->GetNode(iNode));
+            ChVector<> AttachBodyGlobal = Node->GetPos() - L0_t * Node->GetD();  // Locate first the
+            // attachment point in the body in global coordiantes
+            // Stiffness of the Elastic Foundation
+            double K_S = 1.5e9 * NODE_AVE_AREA[iNode];  // Stiffness Constant
+            double C_S = 5e3 * NODE_AVE_AREA[iNode];    // Damper Constant
+            Tottal_stiff += K_S;
+            Tottal_damp += C_S;
+            // Initial length
+            OneLoadSpringDamper->C_dp[iNode] = C_S;
+            OneLoadSpringDamper->K_sp[iNode] = K_S;
+            OneLoadSpringDamper->l0[iNode] = L0_t;
+            // Calculate the damping ratio zeta
+            double zeta, m_ele;
+            m_ele = rho * dz * NODE_AVE_AREA[iNode];
+            zeta = C_S / (2 * sqrt(K_S * m_ele));
+            GetLog() << "Zeta of node # " << iNode << " is set to : " << zeta << "\n";
+            OneLoadSpringDamper->LocalBodyAtt[iNode] = Tibia->Point_World2Body(AttachBodyGlobal);
+        }
+        mloadcontainerGround->Add(OneLoadSpringDamper);
+    }  // End loop over tires
+    my_system.Add(mloadcontainerGround);
+    GetLog() << "Total Stiffness (N/mm)= " << Tottal_stiff / 1e3 << " Total Damping = " << Tottal_damp << "\n";
     // Create the contact surface(s).
     // In this case it is a ChContactSurfaceMesh, that allows mesh-mesh collsions.
     auto mcontactsurf = std::make_shared<ChContactSurfaceMesh>();
@@ -250,8 +401,8 @@ int main(int argc, char* argv[]) {
         for (int NoElmPre = 0; NoElmPre < TotalNumElements; NoElmPre++) {
             auto faceload = std::make_shared<ChLoad<ChLoaderPressure>>(
                 std::static_pointer_cast<ChElementShellANCF>(my_mesh->GetElement(NoElmPre)));
-            faceload->loader.SetPressure(-200);
-            faceload->loader.SetStiff(false);
+            faceload->loader.SetPressure(1e6);
+            faceload->loader.SetStiff(true);
             faceload->loader.SetIntegrationPoints(2);
             Mloadcontainer->Add(faceload);
         }
@@ -282,14 +433,20 @@ int main(int argc, char* argv[]) {
     auto mvisualizemeshcoll = std::make_shared<ChVisualizationFEAmesh>(*(my_mesh.get()));
     mvisualizemeshcoll->SetFEMdataType(ChVisualizationFEAmesh::E_PLOT_CONTACTSURFACES);
     mvisualizemeshcoll->SetWireframe(true);
-    mvisualizemeshcoll->SetDefaultMeshColor(ChColor(1, 1.0, 1));
+    mvisualizemeshcoll->SetDefaultMeshColor(ChColor(0.0, 0.0, 0.0));
     my_mesh->AddAsset(mvisualizemeshcoll);
 
     auto mvisualizemeshbeam = std::make_shared<ChVisualizationFEAmesh>(*(my_mesh.get()));
-    mvisualizemeshbeam->SetFEMdataType(ChVisualizationFEAmesh::E_PLOT_ELEM_STRAIN_VONMISES);
-    mvisualizemeshbeam->SetColorscaleMinMax(-0.0, 0.010);
+    mvisualizemeshbeam->SetFEMdataType(ChVisualizationFEAmesh::E_PLOT_ELEM_STRESS_VONMISES);
+    mvisualizemeshbeam->SetColorscaleMinMax(0.00, 0.070);
     mvisualizemeshbeam->SetSmoothFaces(true);
     my_mesh->AddAsset(mvisualizemeshbeam);
+
+    auto mvisualizemeshDef = std::make_shared<ChVisualizationFEAmesh>(*(my_mesh.get()));
+    mvisualizemeshDef->SetFEMdataType(ChVisualizationFEAmesh::E_PLOT_ANCF_SECTION_DISPLACEMENT);
+    mvisualizemeshDef->SetColorscaleMinMax(0, 0.0009);
+    //    mvisualizemeshDef->SetSmoothFaces(true);
+    my_mesh->AddAsset(mvisualizemeshDef);
 
     auto mvisualizemeshbeamnodes = std::make_shared<ChVisualizationFEAmesh>(*(my_mesh.get()));
     mvisualizemeshbeamnodes->SetFEMglyphType(ChVisualizationFEAmesh::E_GLYPH_NODE_DOT_POS);
@@ -317,34 +474,34 @@ int main(int argc, char* argv[]) {
     // ---------------
     // Simulation loop
     // ---------------
-    ChLcpMklSolver* mkl_solver_stab = new ChLcpMklSolver;
-    ChLcpMklSolver* mkl_solver_speed = new ChLcpMklSolver;
-    my_system.ChangeLcpSolverStab(mkl_solver_stab);
-    my_system.ChangeLcpSolverSpeed(mkl_solver_speed);
-    mkl_solver_stab->SetSparsityPatternLock(true);
-    mkl_solver_speed->SetSparsityPatternLock(true);
-    application.GetSystem()->Update();
+    //    ChLcpMklSolver* mkl_solver_stab = new ChLcpMklSolver;
+    //    ChLcpMklSolver* mkl_solver_speed = new ChLcpMklSolver;
+    //    my_system.ChangeLcpSolverStab(mkl_solver_stab);
+    //    my_system.ChangeLcpSolverSpeed(mkl_solver_speed);
+    //    mkl_solver_stab->SetSparsityPatternLock(true);
+    //    mkl_solver_speed->SetSparsityPatternLock(true);
+    //    application.GetSystem()->Update();
 
     // Setup solver
-    //    my_system.SetLcpSolverType(ChSystem::LCP_ITERATIVE_MINRES);
-    //    ChLcpIterativeMINRES* msolver = (ChLcpIterativeMINRES*)my_system.GetLcpSolverSpeed();
-    //    msolver->SetDiagonalPreconditioning(true);
-    //    my_system.SetIterLCPwarmStarting(true);  // this helps a lot to speedup convergence in this class of
-    //    my_system.SetIterLCPmaxItersSpeed(40);
-    //    my_system.SetTolForce(1e-6);
-    //    msolver->SetVerbose(false);
+    my_system.SetLcpSolverType(ChSystem::LCP_ITERATIVE_MINRES);
+    ChLcpIterativeMINRES* msolver = (ChLcpIterativeMINRES*)my_system.GetLcpSolverSpeed();
+    msolver->SetDiagonalPreconditioning(true);
+    my_system.SetIterLCPwarmStarting(true);  // this helps a lot to speedup convergence in this class of
+    my_system.SetIterLCPmaxItersSpeed(4000000);
+    my_system.SetTolForce(1e-6);
+    msolver->SetVerbose(false);
     //
     // INT_HHT or INT_EULER_IMPLICIT
-    //    my_system.SetIntegrationType(ChSystem::INT_EULER_IMPLICIT_LINEARIZED);  // fast, less precise
+    // my_system.SetIntegrationType(ChSystem::INT_EULER_IMPLICIT_LINEARIZED);  // fast, less precise
 
     my_system.SetIntegrationType(ChSystem::INT_HHT);
     auto mystepper = std::dynamic_pointer_cast<ChTimestepperHHT>(my_system.GetTimestepper());
     mystepper->SetAlpha(-0.2);
     mystepper->SetMaxiters(200);
-    mystepper->SetAbsTolerances(1e-06);
+    mystepper->SetAbsTolerances(1e-06, 1e-3);
     mystepper->SetMode(ChTimestepperHHT::POSITION);
     mystepper->SetScaling(true);
-    mystepper->SetVerbose(false);
+    mystepper->SetVerbose(true);
 
     application.SetTimestep(time_step);
     while (application.GetDevice()->run()) {
